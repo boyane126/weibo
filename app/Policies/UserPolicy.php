1643f4c040f659fc328e32fe6404e2a2d7663dc4
boyane <2628488871@qq.com>
2020-11-08 07:07:08 +0000

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -17,11 +17,16 @@ class UserPolicy
     public function __construct()
     {
         //
-        
+
     }
 
     public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
+    }
+
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }
